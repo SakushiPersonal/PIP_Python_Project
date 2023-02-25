a5b7760c6ebcf6396e07802dae0ffe648eab9a78
sakushi <maranatha.bruno1997@gmail.com>
2023-02-25 15:37:27 -0300

    class13: app/main.py funciones modificadas con panda

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('data_ppl.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
+  df = pd.read_csv('data_ppl.csv')
+  df = df[df['Continent'] == 'North America']
+
+  countries = df['Country'].values
+  percentages = df['world Population Percentage'].values
+  
   charts.generate_pie_chart(countries, percentages)
   
   charts.country_population_chart()
